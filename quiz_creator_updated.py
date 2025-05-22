c4af8e14fcbd8a21538b9f7a1d2321e12dc5340e
Shane <palomoshanecraven@gmail.com>
2025-05-22 12:46:06 +0800

    Import libraries, create a class, and add file location and access it.

--- quiz_creator_updated.py
+++ quiz_creator_updated.py
@@ -1,1 +1,18 @@
-#
+# Import libraries.
+import os
+import json
+
+# Create a class.
+class QuizCreator:
+    
+    # File location.
+    def __init_(self, file_name = "category.json"):
+        self.file_name = file_name
+        self.categories = self.load.categories()
+
+    # Accessing categories
+    def access_categories(self):
+        if os.path.exists(self.file_name):
+            with open(self.file_name, "r") as file:
+                return json.load(file)
+        return {}
