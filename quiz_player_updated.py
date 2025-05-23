f4104c173d8486c9029a598e675259f3e26730a1
Shane <palomoshanecraven@gmail.com>
2025-05-23 13:17:01 +0800

    Pseudocode for this program.

---
+++ quiz_player_updated.py
@@ -0,0 +1,21 @@
+# Import libraries.
+
+# Create a class for history.
+
+    # Define function for history.
+
+    # Define function for recording data.
+
+    # Define function for viewing history.
+
+# Create a class for quiz player.
+
+    # Define function for loading quiz.
+
+    # Define function for playing quiz.
+
+    # Define function for question set.
+
+# Define function for main program (quiz player).
+
+# Run main program.
