e7964427d5a5417465ffdf5ca24a661f5fa4466e
Shane <palomoshanecraven@gmail.com>
2025-05-23 13:34:04 +0800

    Create the main program without the def functions, and change def function name.

--- quiz_player_updated.py
+++ quiz_player_updated.py
@@ -19,11 +19,11 @@ class ViewHistory:
         pass
 
     # Define function for recording data.
-    def save_history(self):
+    def record_data(self):
         pass
 
     # Define function for viewing history.
-    def record_history(self):
+    def view_history(self):
         pass
 
 # Create a class for quiz player.
@@ -46,7 +46,43 @@ class PlayQuiz:
 
 # Define function for main program (quiz player).
 def main_program():
-    pass
+    
+    access_quiz = PlayQuiz()
+    
+    # Print short intro message.
+    print("Hi! This is Quizzo, How can I help you?")
+
+    while True:
+        
+        # Print the selection.
+        print("\nSelection:")
+        print("1. Play a Quiz \n2. View History \n3. Exit \n")
+       
+        try:
+
+            # Allow users to choose from selection.
+            choice = int(input("Enter the number of your choice: "))
+            
+            # If users choose 1, they will be able to play the quiz.
+            if choice == 1:
+                pass
+            
+            # If users choose 2, they will be able to access history.
+            elif choice == 2:
+                pass
+            
+            # If users choose 3, they will be able to exit the program.
+            elif choice == 3:
+                print("\nGoodbye! Thank you for using Quizzo!")
+                break
+           
+            # Catch invalid input.
+            else:
+                print("\nInvalid input! try again.")
+
+        # Catch invalid input.
+        except ValueError:
+            print("\nInvalid input! try again.")
 
 # Run main program.
 if __name__ == "__main__":
